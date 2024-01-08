81b869588dcc779f24106849821223c20162d853
Nuno Ricardo Guimaraes <nunoricardoguimaraes@gmail.com>
2024-01-08 18:44:51 +0000

    Added download script to database

---
+++ webapp/db/download_db.sh
@@ -0,0 +1,6 @@
+echo "Downloading DataBase "
+wget https://drive.inesctec.pt/s/HPDT6orwCiMKaTW/download/PerfilPublicoMongoDB.tar.xz
+echo "Download Complete. Unzipping "
+tar -xf PerfilPublicoMongoDB.tar.xz
+rm PerfilPublicoMongoDB.tar.xz
+
